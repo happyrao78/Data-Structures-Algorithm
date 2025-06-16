2bcb7606037a90969bec9d5e646465f211ddcc4b
Happy Yadav <happy.hy0017@gmail.com>
2025-06-16 11:00:01 +0530

    array:easy level

---
+++ questions/cn/largest.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int large(int arr[],int n){
+    int largest=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    return largest;
+}
+int main(){
+    int size;
+    cout<<"enter size of array:"<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"enter the elements:"<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    cout<<"elements:"<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
+    large(arr,size);
+    return 0;
+}
