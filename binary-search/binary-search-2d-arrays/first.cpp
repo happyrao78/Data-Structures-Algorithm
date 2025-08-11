3def1347ad3efdcaa499e981758df619bc7dcfdb
Happy Yadav <happy.hy0017@gmail.com>
2025-08-11 15:23:05 +0530

    bs:2d arrays

---
+++ binary-search/binary-search-2d-arrays/first.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lower_bound_find(vector<int> &arr,int n,int x){
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid] >= x){
+            high=mid-1;
+        }
+        low=mid+1;
+    }
+    return low;
+}
+
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m){
+    int cntMax=0;
+    int indx=-1;
+    for(auto i=0;i<n;i++){
+        int cnt_ones=m-lower_bound_find(matrix[i],m,1);
+        if(cnt_ones > cntMax){
+            cntMax=cnt_ones;
+            indx=i;
+        }
+    }
+    return indx;
+}
+int main(){
+    int n=3,m=3;
+    vector<vector<int>> arr = {{0,0,1},{0,0,1},{1,1,0}};
+    cout<<rowWithMax1s(arr, n, m);
+    return 0;
+}
