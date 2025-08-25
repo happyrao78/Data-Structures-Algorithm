e95ff6a1805d8d5ad9ae20c25172d2712ddbff46
Happy Yadav <happy.hy0017@gmail.com>
2025-08-26 00:34:09 +0530

    strings:longet prefix

---
+++ strings/strings-tuf/longest-prefix.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    for(int i=0;i<strs[0].size();i++){
+        char c=strs[0][i];
+        for(int j=1;j < strs.size();j++){
+            if(i >= strs[j].size() || strs[j][i] !=c) return strs[0].substr(0,i);
+        }
+    }
+    return strs[0];
+}
+int main(){
+    vector<string> strs={"flower","flow","flight"};
+    cout<<longestCommonPrefix(strs);
+    return 0;
+}
